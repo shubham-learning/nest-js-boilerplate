eac88dbf335de0837ca478c3d901864782718352
Shubham Kumar Tiwari <shubham@Shubham-ka-MacBook-Air.local>
2025-04-22 22:14:17 +0530

    Modify  http-exception filter feature

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,6 @@ import customValidationPipe from 'utils/custom-validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  /**
-   * Helmet can help protect your app from some well-known
-   * web vulnerabilities by setting HTTP headers appropriately.
-   * Generally, Helmet is just a collection of 12 smaller
-   * middleware functions that set security-related HTTP headers
-   *
-   * https://github.com/helmetjs/helmet#how-it-works
-   */
   app.use(helmet());
 
   app.enableCors();
