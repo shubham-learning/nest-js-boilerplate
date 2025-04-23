12c108da0ee50d3c552c917c98fd3dbeee9ebf4b
Shubham Kumar Tiwari <shubham@Shubham-ka-MacBook-Air.local>
2025-04-23 22:18:23 +0530

    remove unwanted code in app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,9 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from 'src/db/data-source';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from 'utils/http-exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
